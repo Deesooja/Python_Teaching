25a9fcd7a64e2ec8745be2424ec6b07a50c51622
Deesooja <deesooja24dp@gmail.com>
2023-07-27 20:17:29 +0530

    Data type learnig

--- data_type.py
+++ data_type.py
@@ -1,7 +1,38 @@
+# for String 
 mithi = "Hello Mithi" 
 
 print(mithi)
 
 variable_type = type(mithi)
 
-print(variable_type)
+print("Data_type of mithi variable ", variable_type)
+
+#  For Integer
+int_variable = 5
+
+int_variable_type = type(int_variable)
+
+print("Data_type of int_variable :-", int_variable_type)
+
+# For Float
+
+float_variable = 1.5
+
+float_variable_data_type = type(float_variable)
+
+print("Data type Of float_variable :- ",float_variable_data_type)
+
+# For Complex Number
+comlex_variable = 1j
+
+comlex_variable_data_type = type(comlex_variable)
+
+print("Data type Of comlex_variable :- ", comlex_variable_data_type)
+
+# For Boolean 
+
+boolean_variable = True
+
+boolean_variable_data_type = type(boolean_variable)
+
+print("data Type Of boolean_variable :- ", boolean_variable_data_type)
